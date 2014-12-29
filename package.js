9363ee0f533c536952d5d4898b0180e532911271
LB (Ben) Johnston <github@lb.ee>
2014-12-29 13:11:02 +1100

    Move package.js & re-work

---
+++ package.js
@@ -0,0 +1,27 @@
+// package metadata file for Meteor.js
+'use strict';
+
+// https://atmospherejs.com/lbee/lunr
+var packageName = 'lbee:lunr';
+
+Package.describe({
+  name: packageName,
+  summary: 'Lunr.js Packaged for Meteor, simple full-text search in your browser.',
+  version: '0.5.6',
+  git: 'https://github.com/lb-/lunr.js.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']);
+  api.export('lunr');
+  api.add_files([
+    'lunr.js',
+    'meteor/export.js',
+  ], ['client', 'server']);
+});
+
+Package.onTest(function (api) {
+  api.use(packageName);
+  api.use('tinytest');
+  api.addFiles('meteor/test.js');
+});
